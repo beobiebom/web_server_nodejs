93c96e6eaf9ff58c935ceca35e14719c1dc9e963
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-06-30 09:46:04 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+//Set path to templates directory 
+const viewsPath = path.join(__dirname, '../templates/views')
+// Set path to partials directory
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// To use file html in public directory
+// index.html in root(head) of app
+app.use(express.static(publicDirectoryPath))
+//To reuse code and handlebar help render dynamic content
+app.set('view engine', 'hbs')
+//default directory of view engine is 'views', to change it then using:
+app.set('views', viewsPath)
+//Register partials
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    //allow render view and can send data to hbs to reuse it
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Nguyen Minh Duc'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Nguyen Minh Duc'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Nguyen Minh Duc'
+    })
+})
+
+// This isn't need because index.html in root
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
+
+// app.get('/help', (req, res) => {
+//     //Auto convert to json
+//     res.send([
+//         {
+//             name: 'Minh Duc'
+//         },
+//         {
+//             name: 'Kim Ngan'
+//         }
+//     ])
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>Abouth1>')
+// })
+
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+})
+
+app.listen(3000, () => {
+    console.log('Server is on port 3000')
+})
