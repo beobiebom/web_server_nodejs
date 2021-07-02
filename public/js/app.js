3b2fc3bd43fda539096630f051333bec129673ab
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-07-02 20:49:30 +0700

    Browser http requests with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side javascript file is loaded!')
+
+// get data from api and using in client side
+fetch('http://localhost:3000/weather?address=vietnam').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.address)
+            console.log(data.forecast)
+        }
+    })
+})
