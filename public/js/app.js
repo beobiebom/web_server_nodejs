51256aa77e64c0cd53d1d03b1f57ae374b1ab2cf
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-07-02 21:14:54 +0700

    Wiring up the User interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,34 @@
 console.log('Client side javascript file is loaded!')
 
-// get data from api and using in client side
-fetch('http://localhost:3000/weather?address=vietnam').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.address)
-            console.log(data.forecast)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) => {
+    //Prevent rerender 
+    event.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageOne.textContent = ''
+
+    // get data from api and using in client side
+    fetch ('http://localhost:3000/weather?address='+location).then(response => {
+        response.json ().then (data => {
+            if (data.error) {
+                // console.log (data.error);
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.address
+                messageTwo.textContent = data.forecast
+                // console.log (data.address);
+                // console.log (data.forecast);
+            }
+        })
     })
+
 })
+
+
